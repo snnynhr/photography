a1fcfb97c11106b39be756f7ce081d8c96709384
Sunny Nahar <snnynhr@users.noreply.github.com>
2020-06-21 22:01:51 -0400

    Add back menu, contact.

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+$('.emgen').on('mouseenter', function(e) {
+    console.log('sdfjlsk');
+    var lace1 = "om";
+    var lace2 = "ynh";
+    var lace3 = "grap";
+    var lace4 = "snn";
+    var lace5 = "hy@gma";
+    var lace6 = "rphoto";
+    var lace7 = "il.c";
+    var text = lace4 + lace2 + lace6 + lace3 + lace5 + lace7 + lace1
+    $('.emgen').text(text).fadeIn();
+});
